0607180223aa1e3ffb2b7d313a23aed5808aa7f4
Ryan Tolentino <Tolentino2016@gmail.com>
2016-07-01 15:20:10 -0400

    Add files via upload

---
+++ USAF CHAIN OF COMMAND.py
@@ -0,0 +1,111 @@
+##Created by Ryan Tolentino
+##@RT_Design
+## May 24,2016
+## Warrior Knowledge
+
+#USAF CHAIN OF COMMAND
+
+wordBank = {'Lt. Gen. Steven L. Kwast',"Barrack Obama","Ashton Carter","Deborah Lee James","General Mark A. Welsh III","Lt. Gen. Darryl Roberson","Brigadier General Paul H. Guemmer","Colonel Sherry L. Stearns-Boles"}
+line = "--------------------------------------------------------------------------------"
+print(wordBank)
+print(line)
+answer = input("Who is the President of the United States? ")
+
+if answer == "Barrack Obama":
+    print("Correct")
+    print(line)
+
+else:
+    print("Incorrect")
+    print("The Correct answer is Barrack Obama ")
+    
+    print(line)
+
+print(wordBank)
+print(line)
+answer = input("Who is the Sec of Defense of the United States? ")
+
+if answer == "Ashton Carter":
+    print("Correct")
+    print(line)
+
+else:
+    print("Incorrect")
+    print("The Correct answer is Ashton Carter ")
+    print(line)
+
+print(wordBank)
+print(line)
+answer = input("Who is the Sec of the Air Force of the United States? ")
+
+if answer == "Deborah Lee James":
+    print("Correct")
+    print(line)
+
+else:
+    print("Incorrect")
+    print("The Correct answer is Deborah Lee James ")
+    print(line)
+
+print(wordBank)
+print(line)
+answer = input("Who is the USAF Chief of Staff of the United States? ")
+
+if answer == "General Mark A. Welsh III":
+    print("Correct")
+    print(line)
+
+else:
+    print("Incorrect")
+    print("The Correct answer is General Mark A. Welsh III ")
+    print(line)
+print(wordBank)
+print(line)
+answer = input("Who is the AETC/CC of the United States? ")
+
+if answer == "Lt. Gen. Darryl Roberson":
+    print("Correct")
+    print(line)
+
+else:
+    print("Incorrect")
+    print("The Correct answer is Lt. Gen. Darryl Roberson ")
+    print(line)
+print(wordBank)
+print(line)
+answer = input("Who is the AU/CC of the United States? ")
+
+if answer == "Lt. Gen. Steven L. Kwast":
+    print("Correct")
+    print(line)
+
+else:
+    print("Incorrect")
+    print("The Correct answer is Lt. Gen. Steven L. Kwast ")
+    print(line)
+print(wordBank)
+print(line)
+answer = input("Who is the Holm Center/CC of the United States? ")
+
+if answer == "Brigadier General Paul H. Guemmer":
+    print("Correct")
+    print(line)
+
+else:
+    print("Incorrect")
+    print("The Correct answer is Brigadier General Paul H. Guemmer ")
+    print(line)
+print(wordBank)
+print(line)
+answer = input("Who is the AFROTC/CC of the United States? ")
+
+if answer == "Colonel Sherry L. Stearns-Boles":
+    print("Correct")
+    print(line)
+
+else:
+    print("Incorrect")
+    print("The Correct answer is Colonel Sherry L. Stearns-Boles")
+    print(line)
+
+
